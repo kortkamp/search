c581ef66d722c84e42eca1d29cf314d980aead57
kortkamp <marcelusmedius@gmail.com>
2023-03-01 07:17:52 -0300

    feat: improve cleaning of data

--- crawlerJS/src/modules/Parser/index.ts
+++ crawlerJS/src/modules/Parser/index.ts
@@ -17,14 +17,22 @@ const parser = (raw: string): IParseResult => {
   const links = root
     .querySelectorAll('a[href]')
     .map(link => link.attributes.href)
-    .filter(link => link.startsWith('http'));
+    .filter(link => link.startsWith('http'))
+    .filter(
+      link =>
+        !(
+          link.endsWith('.pdf') ||
+          link.endsWith('.jpg') ||
+          link.endsWith('.png')
+        ),
+    );
 
   const h1Result = root.querySelectorAll('h1').map(h1 => h1.text);
   const h2Result = root.querySelectorAll('h2').map(h2 => h2.text);
 
   const content = root
-    .querySelectorAll('p, h3, h4 , h5, h6, li')
-    .map(item => item.text)
+    .querySelectorAll('p, h2, h3, h4 , h5, h6, li')
+    .map(item => item.textContent.trim().replace(/\s\s+\t+/g, ' '))
     .filter(item => item !== '' && item !== undefined);
 
   const result = {
