1375ee44a373d101c4cc7e3f9614491ed096d3cf
kortkamp <marcelusmedius@gmail.com>
2023-03-01 07:18:09 -0300

    fix: solves strange chars on data

--- crawlerJS/src/api/WebServices.ts
+++ crawlerJS/src/api/WebServices.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const fetchPage = async (url: string) => {
-  const pageResponse = await axios.get(url, { responseEncoding: 'binary' });
-
+  const pageResponse = await axios.get(url, { responseEncoding: 'utf8' });
   return pageResponse.data;
 };
 
