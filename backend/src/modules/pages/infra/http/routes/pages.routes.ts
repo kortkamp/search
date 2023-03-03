e723f3df7f782d41658dc180d5bc3c0a9bc65976
kortkamp <marcelusmedius@gmail.com>
2023-03-03 12:16:31 -0300

    feat: remove unused imports

--- backend/src/modules/pages/infra/http/routes/pages.routes.ts
+++ backend/src/modules/pages/infra/http/routes/pages.routes.ts
@@ -1,7 +1,4 @@
 import { authCrawler } from '@modules/crawlers/infra/http/middlewares/authCrawler';
-import { authMiddleware } from '@modules/sessions/infra/http/middlewares/authMiddleware';
-import { ensureRoles } from '@modules/users/infra/http/middlewares/ensureRoles';
-import { UserRole } from '@modules/users/models/IUser';
 import { Router } from 'express';
 
 import { PagesController } from '../controllers/PagesController';
